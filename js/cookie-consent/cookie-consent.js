2431a1f0ff5838322dbf7893907506090fc545dc
Sam Bellen <sambellen@MacBook-Pro-van-Sam.local>
2014-01-21 22:25:34 +0100

    Changed event to click

--- js/cookie-consent/cookie-consent.js
+++ js/cookie-consent/cookie-consent.js
@@ -18,10 +18,7 @@ cupcake.cookieconsent = (function($, window, undefined) {
             $('.cookie-bar').addClass('cookie-bar__hide');
         }
 
-        $('.js-cookie-bar__btn').on('touchstart mousedown', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-        }).on('touchend mouseup', function(){
+        $('.js-cookie-bar__btn').on('click', function(){
             document.cookie = 'cupcake-cookie-consent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
 
             $(this).closest('.cookie-bar').addClass('cookie-bar__hide');
