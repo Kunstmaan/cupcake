83cd85b045ec6d473b472e4e5c37029bd9231602
Sam Bellen <sambellen@MacBook-Pro-van-Sam.local>
2014-01-21 20:11:36 +0100

    The cookie consent now checks if there is a cookie, and sets one if the button gets clicked

--- js/cookie-consent/cookie-consent.js
+++ js/cookie-consent/cookie-consent.js
@@ -12,10 +12,18 @@ cupcake.cookieconsent = (function($, window, undefined) {
     var init;
 
     init = function() {
+        var cookie = document.cookie.match(/(?:(?:^|.*;\s*)cupcake\-cookie\-consent\s*\=\s*([^;]*).*$)|^.*$/)[1];
+
+        if (cookie === 'true') {
+            $('.cookie-bar').addClass('cookie-bar__hide');
+        }
+
         $('.js-cookie-bar__btn').on('touchstart mousedown', function(e){
             e.preventDefault();
             e.stopPropagation();
         }).on('touchend mouseup', function(){
+            document.cookie = 'cupcake-cookie-consent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+
             $(this).closest('.cookie-bar').addClass('cookie-bar__hide');
         });
     };
