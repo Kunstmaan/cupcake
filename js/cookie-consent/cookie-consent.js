291d29671a14d14d3d43e8c3ac7844e57b7849b7
Sam Bellen <sambellen@MacBook-Pro-van-Sam.local>
2014-01-21 22:27:34 +0100

    adde preventDefault to the click event

--- js/cookie-consent/cookie-consent.js
+++ js/cookie-consent/cookie-consent.js
@@ -18,7 +18,9 @@ cupcake.cookieconsent = (function($, window, undefined) {
             $('.cookie-bar').addClass('cookie-bar__hide');
         }
 
-        $('.js-cookie-bar__btn').on('click', function(){
+        $('.js-cookie-bar__btn').on('clickx', function(e){
+            e.preventDefault()
+            
             document.cookie = 'cupcake-cookie-consent=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
 
             $(this).closest('.cookie-bar').addClass('cookie-bar__hide');
